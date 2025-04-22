de9cf094a0a6e6aabcf873c1322317c00cb9c495
Vinul <jameszvinul@gmail.com>
2025-04-23 01:59:04 +0530

    user controller bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existingUser = User.findOne({
+    const existingUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -31,17 +31,18 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    let coverImageLocalPath
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
-
+ 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-
-    if(coverImageLocalPath) {
-        const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-    }
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar) {
         throw new ApiResponse(400, "Avatar file is required")
