1b5210cb68d2b5efd69a7b99b4dffd39978c9179
Vinul <jameszvinul@gmail.com>
2025-04-26 13:20:11 +0530

    subscription model added

---
+++ src/models/subscription.model.js
@@ -0,0 +1,22 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const subscriptionSchema = new Schema(
+    {
+        subscriber: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+        channel: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    },
+    {
+        timestamps: true
+    }
+)
+
+subscriptionSchema.plugin(mongooseAggregatePaginate)
+
+export const Susbcription = mongoose.model("Subscription", subscriptionSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,4 @@
-import mongoose, {mongo, Schema} from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
@@ -33,7 +33,7 @@ const videoSchema = new Schema(
         },
         owner: {
             type: Schema.Types.ObjectId,
-            ref: "User   "
+            ref: "User"
         },
     },
     {
